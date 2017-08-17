04c546c869d8adb794feb1813b1f608a5317ebc2
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-08-17 15:33:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,7 @@
 console.log('Loaded!');
+
 var img = document.getElementById('img');
-img.onclick = function(){
-  img.style.marginright = '100px';  
+img.onclick = function()
+{
+  img.style.marginleft = '100px';  
 };
