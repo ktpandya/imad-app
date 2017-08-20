4ea4854719db4396b64b7c08939b828813d205e5
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-08-20 15:59:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,8 @@ var marginleft = 0;
 function moveright ()
 {marginleft = marginleft + 10;
 madi.style.marginleft = marginleft + 'px';
-    
 }
+img.onclick = function ()
+{
+var interval = setinterval(moveright,100);    
+};
