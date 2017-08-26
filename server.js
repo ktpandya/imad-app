97e991a2264f8be46102e692ace6c14e5a88e33b
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-08-26 11:39:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-/*var title;
+var title;
 var heading;
 var content;
 
