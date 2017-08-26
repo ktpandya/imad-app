45a85e93b5a14389b7e68f29ce9c66a6f309b70f
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-08-26 18:44:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,5 +11,19 @@ var interval = setinterval(moveright,100);
 madi.onclick = function ()
 {marginleft = marginleft + 10;
 madi.style.marginleft = '10px';
-    
+
+};
+var nameinput = document.getElementById('name');
+var submit = document.getElementById('submit');
+var name = nameinput.value;
+submit.onclick = function ()
+{
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for (var i = 0; i < names.length; i++);
+    {
+    list = '<li>' + name[i] + '</li>' ;
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
