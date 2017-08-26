6c225622f566ab659bde969663b5ae23695ed786
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-08-26 19:08:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,4 @@
-/*var madi = document.getElementById('madi');
-var marginleft = 0;
-/*function moveright ()
-{marginleft = marginleft + 10;
-madi.style.marginleft = marginleft + 'px';
-}
-madi.onclick = function ()
-{
-var interval = setinterval(moveright,100);    
-};
-madi.onclick = function ()
-{marginleft = marginleft + 10;
-madi.style.marginleft = '10px';
-
-};*/
-var nameinput = document.getElementById('name_');
+var nameinput = document.getElementById('name');
 var submit = document.getElementById('submit');
 var name = nameinput.value;
 var request = new XMLHttpRequest();
