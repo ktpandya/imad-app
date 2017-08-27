cf7e73316c96074dab343d5c4d251971fe1ff704
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-08-27 19:46:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-/*var pool = require('pg').pool;
+var pool = require('pg').pool;
 var config={
     user:'kushpandya69',
     database:'kushpandya69',
     host:'DB-imad.hasura-app.io',
     port:5432,
     password:process.env.DB_PASSWORD,
-};*/
-    
-
+};
 var app = express();
 app.use(morgan('combined'));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-
-var articles=
-{
+var articles={
     articleOne:
     {
     title : 'KUSH PANDYA ARTICLE 1',
@@ -86,10 +80,7 @@ var articles=
         
                  },
 };
-
-
-function createtemplate(data)
-{
+function createtemplate(data){
     var title = data.title;
     var heading = data.heading;
     var content = data.content;
@@ -113,10 +104,8 @@ function createtemplate(data)
     `;
     return HTMLtemplate;
 }
-
-/*var pool = new pool(config);
-app.get('/test-DB',function (req,res)
-{    pool.query('SELECT * FROM test',function(err,result){
+var pool = new pool(config);
+app.get('/test-DB',function (req,res){    pool.query('SELECT * FROM test',function(err,result){
          if (err)
         {res.status(500).send(err,toString);
          
@@ -126,25 +115,21 @@ app.get('/test-DB',function (req,res)
          
      }
     });
-}
-);*/
- app.get('/:articleName',function (req,res){
+});
+app.get('/:articleName',function (req,res){
     var articleName = req.params.articleName;
     res.send(createtemplate(articles[articleName]))});
-/*var counter = 0;
-app.get('/counter', function (req, res)
-{
+var counter = 0;
+app.get('/counter', function (req, res){
 counter = counter + 1;
 res.send(counter.toString());
-});*/
-
-app.get('/ui/style.css', function (req, res) {
+});
+app.get('/ui/style.css', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
