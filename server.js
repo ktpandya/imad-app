3cda079d5ebcc30d54ca9c80d61b92085377bec4
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-08-30 19:36:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/articles/articleName',function (req,res){
             else
             {var articleData = result.rows[0];
             
-                res.send(createTemplate(articledata));
+                res.send(createTemplate(articles[articledata]));
             }
      }
     });
