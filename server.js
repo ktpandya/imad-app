71ea501855f144ab9890cb8ea954ac5e6f5c9b10
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-09-01 15:10:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
+var bodyparser = require('body-parser');
 var Pool = require('pg').Pool;
 var config={
     user : 'kushpandya69',
@@ -11,7 +12,7 @@ var config={
     password: process.env.DB_PASSWORD,
 };
 var app = express();
-
+app.use(bodyparser.JSON());
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -110,11 +111,26 @@ function hash (input , salt)
 {var hashed = crypto.pbkdf2Sync(input , salt ,10000 , 512, 'sha512');
 return ['pbkdf2' , '10000' , salt ,hashed.toString('hex')].join('_');
 }
-app.get('/hash/:input' , function (req , res)
-
-{
+app.get('/hash/:input' , function (req , res){
     var hashedString = hash (req.params.input , 'a random value');
     res.send(hashedString);
+});
+app.post('/createuser' , function(req,res)
+{
+var username = req.body.username;
+var password = req.body.password;
+var salt = getRandomBytes(128).toString('hex');
+var dbString =hash(password , salt);
+pool.query('INSERT INTO "user" (username,password) VALUES ($1 , $2)' , [username , dbString] , function (req,res)
+{
+    if (err)
+   {res.status(500).send(err,toString());
+      }
+    else
+    {   res.send('User succesfully created');}
+});
+
+    
 });
 
 var pool = new Pool(config);
