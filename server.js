a55418cf55b340df04b9782dbdcad0f21339d918
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-09-02 11:52:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-var pool = require('pg').pool;
+var Pool = require('pg').Pool;
 var config={
     user : 'kushpandya69',
     database : 'kushpandya69',
@@ -12,7 +12,7 @@ var config={
     password: process.env.DB_PASSWORD,
 };
 var app = express();
-var pool = new pool(config);
+var pool = new Pool(config);
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
@@ -57,8 +57,7 @@ var username = req.body.username;
 var password = req.body.password;
 var salt = crypto.randomBytes(128).toString('hex');
 var dbString =hash(password , salt);
-pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err,result)
-{    if (err)
+pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err,result){    if (err)
    {res.status(500).send(err,toString());
       }
      else
