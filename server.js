fc62ece0c5fb5b7e94124e6732a96f069ac3c718
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-09-04 22:06:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -207,7 +207,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
-/*app.get('/articles/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
     if (err) {
@@ -215,11 +215,10 @@ app.post('/submit-comment/:articleName', function (req, res) {
     else {
         if (result.rows.length === 0) {
             res.status(404).send('Article not found');} 
-            else {
+        else {
             var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
-        }}}};)
-    */
+            res.send(createTemplate(articleData));}
+})};
   
 
 
