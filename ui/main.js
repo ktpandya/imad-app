3325ad88afeca347cc4abb19fe5b7b3cf7b4dbaf
kushpandya69 <30921954+ktpandya@users.noreply.github.com>
2017-09-06 20:18:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
-
+/*var username = document.getElementById('username');
+username.onclick.function(result);
+{
+pool.query(`SELECT * FROM "user" WHERE "username" = ` , username , function (err, result)
+{
+    if (err)
+        {}
+    else
+    {pool.query(`SELECT * FROM "user" WHERE "password" = ` , password , function (err,result)
+    {if (err)
+        {res.send('error');}
+    else
+    {}
+    })}}*/
